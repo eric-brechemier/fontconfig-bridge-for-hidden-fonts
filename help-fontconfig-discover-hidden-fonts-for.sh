4fca6e784028333667984bf64e6d1befe5da6974
Eric Bréchemier <eric@bezen.org>
2021-04-15 19:24:55 +0200

    Add first version of the script
    This first version of the script creates hard links, not symbolic links.
    The links are stored in ~/.fonts, which is a deprecated location which
    will be removed in the future, according to a comment in the Fontconfig
    configuration. And it differentiates services by adding a custom extension
    to the name of each link file instead of using a separate subfolder.
    
    I am adding successive versions of the script for historical purpose.
    I have corrected each of these shortcomings in a later version.
    
    This script is similar to the version I used in February 2020.

---
+++ help-fontconfig-discover-hidden-fonts-for.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# Help Fontconfig to discover hidden fonts
+#
+# Parameters:
+#   * name: string, used in file names to differentiate each font service
+#   * cache: path, location of the parent folder of hidden fonts
+#
+fontServiceName="$1"
+fontServiceCache="$2"
+
+if test -z "$fontServiceName" -o -z "$fontServiceCache"
+then
+  echo "Usage: $0 name cache" 1>&2
+  exit 1
+fi
+
+echo 'Help Fontconfig to discover hidden fonts'
+
+echo 'Create Fontconfig folder if missing'
+fontconfigFolder="$HOME/.fonts"
+mkdir -p "$fontconfigFolder" || exit 1
+cd "$fontconfigFolder" || exit 2
+
+echo 'Reset'
+rm *."$fontServiceName".otf 2>/dev/null
+
+for file in "$fontServiceCache"/.*
+do
+  # extract file name without extension
+  name="$(basename "$file" '.otf')"
+  # remove initial '.', add custom extension
+  name="${name#.}."$fontServiceName".otf"
+  # create link
+  echo "$name -> $file"
+  ln "$file" "$name"
+done
+
+echo 'Refresh font cache'
+fc-cache -v || exit 3
+
+echo 'Done.'
